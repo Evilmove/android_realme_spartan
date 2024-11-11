1deacfef8cd8ff3e11612495ecbd40022b09e6dd
Evilmove <evilmove4378@gmail.com>
2024-11-11 09:50:22 +0530

    switch to richtap vibrator

--- device.mk
+++ device.mk
@@ -482,7 +482,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.oplus
+    android.hardware.vibrator-V1-ndk_platform.vendor
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
@@ -534,3 +534,7 @@ PRODUCT_PACKAGES += \
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/realme/spartan/spartan-vendor.mk)
+
+# InnerTune
+PRODUCT_PACKAGES += \
+    InnerTune
