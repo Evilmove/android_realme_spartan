c0ca0bad32da18b935097f46ccf33a3d8c67df58
Evilmove <evilmove4378@gmail.com>
2025-01-10 13:52:31 +0530

    adding vendersetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/Evilmove/hardware_oplus.git -b vos hardware/oplus
+git clone https://github.com/Evilmove/proprietary_vendor_realme_spartan_A15.git -b 15 vendor/realme/spartan
+git clone https://github.com/ryukftw/android_kernel_realme_sm8250.git -b phoeniX-AOSP kernel/realme/sm8250
